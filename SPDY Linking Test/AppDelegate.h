57ade6f8d8e85b3b82f688835706f9bfb24399a1
Daniel Tull <dt@danieltull.co.uk>
2014-02-20 18:28:45 +0100

    Initial Commit

---
+++ SPDY Linking Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SPDY Linking Test
+//
+//  Created by Daniel Tull on 20/02/2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SPDY Linking TestTests/SPDY_Linking_TestTests.m
@@ -0,0 +1,34 @@
+//
+//  SPDY_Linking_TestTests.m
+//  SPDY Linking TestTests
+//
+//  Created by Daniel Tull on 20/02/2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SPDY_Linking_TestTests : XCTestCase
+
+@end
+
+@implementation SPDY_Linking_TestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
